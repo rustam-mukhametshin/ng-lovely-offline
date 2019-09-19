5a8f9797fcc4c253e9487e0611f03fcaa992fc4c
iproman <itechman@yandex.ru>
2019-09-19 20:18:36 +0500

    Add angular firebase support of database and authentication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
@@ -18,7 +22,9 @@ import {environment} from '../environments/environment';
     MatToolbarModule,
     MatIconModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // needed for database features
+    AngularFireAuthModule, // needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]

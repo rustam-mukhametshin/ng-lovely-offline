c47d3a62f177943785787ef6d2f2ab8dbd7e261d
iproman <itechman@yandex.ru>
2019-09-20 18:57:37 +0500

    Generate footer and header components.

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
 })
-export class LayoutModule { }
+export class LayoutModule {
+}
